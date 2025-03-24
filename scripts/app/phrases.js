f9de932ca21b3de583fa1ce493aa5de0cd660384
TallishHobbit <167260218+TallishHobbit@users.noreply.github.com>
2025-03-24 07:11:01 -0500

    Added app folder, app logic
    Task-specific code (phrases.js), separate from JS interactivity (app.js)

---
+++ scripts/app/phrases.js
@@ -0,0 +1,10 @@
+// TODO: Make a one-time-use function to lemmatize the phrases
+// and make lists of identifying words/phrases. Then, calculate
+// any connections between them.
+
+define(["lemmatizer"], function(lem) {
+  let lemmatizer = new lem.Lemmatizer();
+});
+
+// TODO: Write a function that uses the lemmatizer on the given doc
+// and searches phrases.json for matches.
