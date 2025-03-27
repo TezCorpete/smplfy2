1a2b39ab2182754c4eb79e2cf70120d34bc59af2
TallishHobbit <167260218+TallishHobbit@users.noreply.github.com>
2025-03-27 13:30:05 -0500

    Added display: flex to phrase div

--- scripts/app.js
+++ scripts/app.js
@@ -115,7 +115,6 @@ function (  $,        phrases) {
     }
 
     $("#def-tbl-phrase").text( phraseData.phrase );
-    $("#def-tbl-phrase").css("text-align", "center");
 
     $("#def-tbl-def").text( phraseData.meaning );
   }
