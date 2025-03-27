e5e6a38e9295dc84aa5c3802c99515ab22f2e0c5
TallishHobbit <167260218+TallishHobbit@users.noreply.github.com>
2025-03-27 12:57:18 -0500

    Formatting tweaks

--- scripts/app.js
+++ scripts/app.js
@@ -108,14 +108,10 @@ function (  $,        phrases) {
       $("#def-tbl-cat").text( phraseData.category );
 
       $("def-tbl-phrase").css("grid-row", "3 / span 1");
-      
-      $("def-tbl-def").css("grid-row", "2 / span 2");
     } else {
       $("def-tbl-cat").hide();
 
-      $("def-tbl-phrase").css("grid-row", "2 / span 1");
-
-      $("def-tbl-def").css("grid-row", "2 / span 1");
+      $("def-tbl-phrase").css("grid-row", "2 / span 2");
     }
 
     $("#def-tbl-phrase").text( phraseData.phrase );
