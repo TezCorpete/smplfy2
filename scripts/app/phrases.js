628072b288ab9599e1eb4ae33da1fe98f62a77ad
TallishHobbit <167260218+TallishHobbit@users.noreply.github.com>
2025-03-30 16:14:13 -0500

    Added handling for undefined return from lemmatizer

--- scripts/app/phrases.js
+++ scripts/app/phrases.js
@@ -240,7 +240,7 @@ function(lem,          phraseData,              lookupData) {
     
     // Get one lemma for each word (Only ever choosing the first lemma should be fine, right?)
     const normalized = capNormalized.map( (word) => {
-      const normWord = lemmatizer.only_lemmas(word)[0]; // ".only_lemmas" returns a list
+      const normWord = lemmatizer.only_lemmas( word )[0]; // ".only_lemmas" returns a list
       
       // Acronyms are always to be left as is, and
       // Lemmatizer tends to delete one letter words,
@@ -248,7 +248,7 @@ function(lem,          phraseData,              lookupData) {
       // Shorter words in general aren't treated well,
       // hence the extra check.
       if ( word.isAcronym() || word.length == 1 
-      || normWord.length == 0 || normWord.valueOf() == " " ) {
+      || normWord === undefined || normWord.length == 0 || normWord.valueOf() == " " ) {
         return word;
       }
       // Otherwise, it's business as usual
